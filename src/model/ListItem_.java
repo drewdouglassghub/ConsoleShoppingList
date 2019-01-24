3bc96462782c44af1233e8fd81025f3a4e545ee3
drewdouglassghub <44046707+drewdouglassghub@users.noreply.github.com>
2019-01-24 01:01:32 -0600

    initial commit
    tutorial
    mysql

---
+++ src/model/ListItem_.java
@@ -0,0 +1,13 @@
+package model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2019-01-24T00:13:29.312-0600")
+@StaticMetamodel(ListItem.class)
+public class ListItem_ {
+	public static volatile SingularAttribute<ListItem, Integer> id;
+	public static volatile SingularAttribute<ListItem, String> store;
+	public static volatile SingularAttribute<ListItem, String> item;
+}
